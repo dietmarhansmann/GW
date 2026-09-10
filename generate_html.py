d037434bd8b0f26c7add6bc18b902ca3ffa19470
dietmar.hansmann <dietmar.hansmann01_EXT@sonepar.de>
2026-09-10 09:51:55 +0200

    feat: update Marschollek rule to 50% Doppel (ratio: 0.5) and regenerate HTML

--- generate_html.py
+++ generate_html.py
@@ -150,8 +150,8 @@ PLAYER_RULES = {
             {
                 "id": "slot_preference_marschollek",
                 "target": "doppel_pref",
-                "ratio": 0.2,
-                "description": "Möchte ca. 20% Doppel spielen."
+                "ratio": 0.5,
+                "description": "Möchte ca. 50% Doppel spielen."
             },
             {
                 "id": "blackout_spieltage",
